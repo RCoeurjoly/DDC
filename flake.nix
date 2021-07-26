a98325b47298b0e2a4991a2d96cdb0cacf078125
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2021-07-26 22:23:30 +0200

    WIP

--- flake.nix
+++ flake.nix
@@ -1,21 +1,21 @@
 {
-  description = "A flake for building quicksort";
+    description = "A flake for building quicksort";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    };
 
-  outputs = { self, nixpkgs }: {
+    outputs = { self, nixpkgs }: {
 
-    defaultPackage.x86_64-linux =
-      # Notice the reference to nixpkgs here.
-      with import nixpkgs { system = "x86_64-linux"; };
-      stdenv.mkDerivation {
-        name = "quicksort";
-        src = self;
-        buildPhase = "gcc -g -o quicksort ./quicksort.cpp";
-        installPhase = "mkdir -p $out/bin; install -t $out/bin quicksort";
-      };
+        defaultPackage.x86_64-linux =
+# Notice the reference to nixpkgs here.
+            with import nixpkgs { system = "x86_64-linux"; };
+        stdenv.mkDerivation {
+            name = "quicksort";
+            src = self;
+            buildPhase = "gcc -g -o quicksort ./quicksort.cpp -lstdc++";
+            installPhase = "mkdir -p $out/bin; install -t $out/bin quicksort";
+        };
 
-  };
+    };
 }
