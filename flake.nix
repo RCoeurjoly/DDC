85088437aede357abbf9aef7119c6046059337f0
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2021-07-26 22:29:32 +0200

    WIP

--- flake.nix
+++ flake.nix
@@ -12,6 +12,7 @@
             with import nixpkgs { system = "x86_64-linux"; };
         stdenv.mkDerivation {
             name = "quicksort";
+            buildInputs = [ gdb python3 rr];
             src = self;
             buildPhase = "gcc -g -o quicksort ./quicksort.cpp -lstdc++";
             installPhase = "mkdir -p $out/bin; install -t $out/bin quicksort";
