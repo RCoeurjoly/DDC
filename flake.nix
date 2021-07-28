da0931d5382d21b9b03adb03d39ae58b98438074
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2021-07-28 20:57:04 +0200

    WIP

--- flake.nix
+++ flake.nix
@@ -1,25 +1,27 @@
 {
-    description = "A flake for building quicksort";
+  description = "A flake for building quicksort";
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    };
-    outputs = { self, nixpkgs }: {
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
 
-        defaultPackage.x86_64-linux =
-# Notice the reference to nixpkgs here.
-            with import nixpkgs { system = "x86_64-linux"; };
-        stdenv.mkDerivation {
-            name = "quicksort";
-            src = self;
-            buildPhase = "gcc -g -o quicksort ./quicksort.cpp -lstdc++";
-            installPhase = "mkdir -p $out/bin; install -t $out/bin quicksort";
-        };
+  outputs = { self, nixpkgs }: {
 
-        devShell.x86_64-linux =
-          nixpkgs.legacyPackages.x86_64-linux.mkShell { buildInputs = [ nixpkgs.legacyPackages.x86_64-linux.gdb
-                                                                        nixpkgs.legacyPackages.x86_64-linux.python3
-                                                                        nixpkgs.legacyPackages.x86_64-linux.rr ]; };
+    defaultPackage.x86_64-linux =
+      # Notice the reference to nixpkgs here.
+      with import nixpkgs { system = "x86_64-linux"; };
+      stdenv.mkDerivation {
+        name = "quicksort";
+        src = self;
+        dontStrip = true;
+        buildPhase = "gcc -g -o quicksort ./quicksort.cpp -lstdc++";
+        installPhase = "mkdir -p $out/bin; install -t $out/bin quicksort";
+      };
 
-    };
+    devShell.x86_64-linux =
+      nixpkgs.legacyPackages.x86_64-linux.mkShell { buildInputs = [ nixpkgs.legacyPackages.x86_64-linux.gdb
+                                                                    nixpkgs.legacyPackages.x86_64-linux.python3
+                                                                    nixpkgs.legacyPackages.x86_64-linux.rr ]; };
+
+  };
 }
