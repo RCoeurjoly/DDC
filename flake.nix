b1ad1430305486e0beefe9f700b43819edac57a5
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2022-01-21 22:45:22 +0100

    Shell with the poetry project and the needed tools (gdb, rr etc)

--- flake.nix
+++ flake.nix
@@ -11,6 +11,13 @@
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
 
+      myAppEnv = pkgs.poetry2nix.mkPoetryEnv {
+        projectDir = ./.;
+        editablePackageSources = {
+          my-app = ./src;
+        };
+      };
+
       customOverrides = self: super: {
         # Overrides go here
         tomli = super.tomli.overridePythonAttrs (
@@ -117,6 +124,17 @@
           installPhase = "mkdir -p $out/tests; install -t $out/tests fibonacci";
         };
 
+      packages.x86_64-linux.BST =
+        # Notice the reference to nixpkgs here.
+        with import nixpkgs { system = "x86_64-linux"; };
+        stdenv.mkDerivation {
+          name = "BST";
+          src = self;
+          dontStrip = true;
+          buildPhase = "gcc -O0 -g -o BST ./BST.cpp -lstdc++";
+          installPhase = "mkdir -p $out/tests; install -t $out/tests BST";
+        };
+
       packages.x86_64-linux.quicksort_array =
         # Notice the reference to nixpkgs here.
         with import nixpkgs { system = "x86_64-linux"; };
@@ -129,10 +147,15 @@
         };
 
 
-      devShell.x86_64-linux = pkgs.mkShell {
-        inputsFrom = builtins.attrValues self.packages.x86_64-linux;
+
+      # devShell.x86_64-linux = pkgs.mkShell {
+      #   inputsFrom = builtins.attrValues self.packages.x86_64-linux;
+      #   buildInputs = with pkgs; [ python38 gdb rr z3 boogie poetry python38Packages.pylint python38Packages.autopep8 ];
+      # };
+
+      devShell.x86_64-linux = myAppEnv.env.overrideAttrs (oldAttrs: {
         buildInputs = with pkgs; [ python38 gdb rr z3 boogie poetry python38Packages.pylint python38Packages.autopep8 ];
-      };
+      });
 
       checks.x86_64-linux = {
 
--- quicksort.h
+++ quicksort.h
@@ -1,32 +1,21 @@
-/* C implementation QuickSort */
-#include <stdio.h>
 #include <iostream>
 #include <vector>
 
-// A utility function to swap two elements
 void swap(int* a, int* b)
 {
   if (*a != 10)
     std::swap(*a, *b);
 }
 
-/* This function takes last element as pivot, places
-   the pivot element at its correct position in sorted
-   array, and places all smaller (smaller than pivot)
-   to left of pivot and all greater elements to right
-   of pivot */
 int partition(std::vector<int> &my_vector, int low, int high)
 {
-  int pivot = my_vector[high];    // pivot
-  int i = (low - 1);  // Index of smaller element
-
+  int pivot = my_vector[high];
+  int i = (low - 1);
   for (int j = low; j <= high - 1; j++)
     {
-      // If current element is smaller than or
-      // equal to pivot
       if (my_vector[j] <= pivot)
         {
-          i++;    // increment index of smaller element
+          i++;
           if (i != j)
             swap(&my_vector[i], &my_vector[j]);
         }
@@ -36,20 +25,11 @@ int partition(std::vector<int> &my_vector, int low, int high)
   return (i + 1);
 }
 
-/* The main function that implements QuickSort
-   my_vector --> vector to be sorted,
-   low  --> Starting index,
-   high  --> Ending index */
 void quickSort(std::vector<int> &my_vector, int low, int high, std::string gdb_bug="")
 {
   if (low < high)
     {
-      /* pi is partitioning index, my_vector[p] is now
-         at right place */
       int pi = partition(my_vector, low, high);
-
-      // Separately sort elements before
-      // partition and after partition
       quickSort(my_vector, low, pi - 1);
       quickSort(my_vector, pi + 1, high);
     }
