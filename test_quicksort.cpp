a5ebec6ba13a13f5741bc1501607a760962f34df
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2022-07-07 15:38:52 +0200

    WIP

--- test_quicksort.cpp
+++ test_quicksort.cpp
@@ -16,5 +16,14 @@ int main()
     if (!std::is_sorted(my_vector.begin(), my_vector.end()))
       return 1;
   }
+  my_vector[0] = 7;
+  my_vector[1] = 1;
+  my_vector[2] = rand() % 9 + 1;
+  my_vector[3] = rand() % 9 + 1;
+  my_vector[4] = rand() % 9 + 1;
+  my_vector[5] = rand() % 9 + 1;
+  quickSort(my_vector, 0, my_vector.size()-1, "");
+  if (!std::is_sorted(my_vector.begin(), my_vector.end()))
+    return 1;
   return 0;
 }
